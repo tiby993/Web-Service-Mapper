b7315da7b5537b0c1cccf2fa93af1a04622c9ff7
Tiby993 <turaitibor93@gmail.com>
2023-07-30 02:53:11 +0200

    Save results to json or csv.

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Web-Services-Mapper
+
+"web-service-mapper" is a Go program that maps various details of web services from a CSV file containing domain names. It can process the data concurrently using multiple threads specified by the user. The program resolves the IP addresses of the domains and fetches HTTP headers and response bodies from the ports 80 (HTTP) and 443 (HTTPS) to determine if the web service is a WordPress site or if it performs redirects. The results can be displayed on the console, saved in CSV format, or exported to a JSON file based on user preferences. Additionally, the program supports subdomain CSV files, enabling it to process subdomains and their associated IP addresses without DNS resolution.
+
+
+## Usage
+
+```
+./main -file domains.csv
+```
+## Switches (Flags)
+
+-file / --file: This switch is used to specify the path to the CSV file containing the domain names. It is a required switch, and without it, the program will not know which CSV file to process.
+
+Example usage:
+```
+./main -file domains.csv
+```
+
+-threads / --threads: This switch allows you to set the number of threads the program should use for concurrent processing. The program will process the domains concurrently to improve performance. The default value is 1 if not specified.
+
+Example usage:
+```
+./main -file domains.csv -threads 4
+```
+
+-o / --output: This switch is used to specify the output file name or path where the results will be saved. It can be used for both CSV and JSON formats.
+
+Example usage (saving results in CSV format):
+```
+./main -file domains.csv -o output.csv
+```
+
+Example usage (saving results in JSON format):
+```
+./main -file domains.csv -json -o output.json
+```
+
+-sf / --sf: This switch indicates that the CSV file contains subdomains instead of just domain names. It allows the program to process subdomain CSV files correctly.
+
+Example usage (with a subdomain CSV file):
+```
+./main -sf -file subdomains.csv
+```
+
+-json / --json: This switch is used to specify that the results should be saved in JSON format. It can be combined with the -o switch to specify the output file name.
+
+Example usage:
+```
+./main -file domains.csv -json -o output.json
+```
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net"
+	"io"
 	"net/http"
 	"os"
 	"strings"
@@ -13,27 +13,45 @@ import (
 )
 
 func main() {
-	filePath := flag.String("file", "domains.csv", "The path to the domains CSV file")
+	filePath := flag.String("file", "", "The path to the CSV file")
 	numThreads := flag.Int("threads", 1, "Number of threads in the processing")
+	outputFile := flag.String("o", "", "Output file name")
+	subdomainFlag := flag.Bool("sf", false, "Indicates whether the CSV file contains subdomains")
+	jsonOutputFlag := flag.Bool("json", false, "Save results in JSON format")
 	flag.Parse()
+
+	if *subdomainFlag && *filePath == "" {
+		fmt.Println("Error: the path to the CSV file is required when using the -sf flag")
+		return
+	}
 
 	if *numThreads <= 0 {
 		fmt.Println("Error: the number of threads must be greater than 0")
 		return
 	}
 
-	file, err := os.Open(*filePath)
-	if err != nil {
-		fmt.Println("Error when opening the file:", err)
-		return
-	}
-	defer file.Close()
+	var records [][]string
+	if *subdomainFlag {
+		var err error
+		records, err = readSubdomainCSV(*filePath)
+		if err != nil {
+			fmt.Println("Error reading subdomain CSV file:", err)
+			return
+		}
+	} else {
+		file, err := os.Open(*filePath)
+		if err != nil {
+			fmt.Println("Error when opening the file:", err)
+			return
+		}
+		defer file.Close()
 
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Error when reading CSV file:", err)
-		return
+		reader := csv.NewReader(file)
+		records, err = reader.ReadAll()
+		if err != nil {
+			fmt.Println("Error when reading CSV file:", err)
+			return
+		}
 	}
 
 	domains := make(chan string, len(records))
@@ -54,25 +72,25 @@ func main() {
 			defer wg.Done()
 
 			for domain := range domains {
-				ips, err := resolveIP(domain)
-				if err != nil {
+				ips := getIPsFromRecord(domain, *subdomainFlag, records)
+				if len(ips) == 0 {
 					results <- fmt.Sprintf("%s; N/A; N/A; N/A; N/A", domain)
 					continue
 				}
 
 				for _, ip := range ips {
-					headers, err := fetchHeaders(domain, 80)
+					headers, err := fetchHeaders(domain, false)
 					if err != nil {
-						headers, err = fetchHeaders(domain, 443)
+						headers, err = fetchHeaders(domain, true)
 						if err != nil {
 							results <- fmt.Sprintf("%s; %s; N/A; N/A; N/A", domain, ip)
 							continue
 						}
 					}
 
-					body, err := fetchResponseBody(domain, 80)
+					body, err := fetchResponseBody(domain, false)
 					if err != nil {
-						body, err = fetchResponseBody(domain, 443)
+						body, err = fetchResponseBody(domain, true)
 						if err != nil {
 							results <- fmt.Sprintf("%s; %s; %s; N/A; N/A", domain, ip, headers.Get("Server"))
 							continue
@@ -101,22 +119,141 @@ func main() {
 		close(results)
 	}()
 
+	if *jsonOutputFlag {
+		err := saveResultsToJSON(*outputFile, results)
+		if err != nil {
+			fmt.Println("Error saving results to JSON file:", err)
+		} else {
+			fmt.Println("Results saved to", *outputFile)
+		}
+	} else if *outputFile == "" {
+		printResultsToConsole(results)
+	} else {
+		err := saveResultsToCSV(*outputFile, results)
+		if err != nil {
+			fmt.Println("Error saving results to CSV file:", err)
+		} else {
+			fmt.Println("Results saved to", *outputFile)
+		}
+	}
+}
+
+func printResultsToConsole(results chan string) {
 	fmt.Println("Domain; IP Addr; Header; WP; Redirect")
 	for result := range results {
 		fmt.Println(result)
 	}
 }
 
-func resolveIP(domain string) ([]string, error) {
-	ips, err := net.LookupHost(domain)
+func saveResultsToCSV(outputFile string, results chan string) error {
+	file, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	outputWriter := csv.NewWriter(file)
+	defer outputWriter.Flush()
+
+	headers := []string{"Domain", "IP Addr", "Header", "WP", "Redirect"}
+	outputWriter.Write(headers)
+
+	for result := range results {
+		data := strings.Split(result, "; ")
+		outputWriter.Write(data)
+	}
+
+	return nil
+}
+
+func saveResultsToJSON(outputFile string, results chan string) error {
+	file, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var jsonData []map[string]string
+
+	for result := range results {
+		data := strings.Split(result, "; ")
+		record := map[string]string{
+			"Domain":   data[0],
+			"IP Addr":  data[1],
+			"Header":   data[2],
+			"WP":       data[3],
+			"Redirect": data[4],
+		}
+		jsonData = append(jsonData, record)
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(jsonData); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readSubdomainCSV(filePath string) ([][]string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
-	return ips, nil
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	var records [][]string
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		records = append(records, record)
+	}
+
+	return records, nil
 }
 
-func fetchHeaders(domain string, port int) (http.Header, error) {
-	response, err := http.Head(fmt.Sprintf("http://%s:%d", domain, port))
+func getIPsFromRecord(domain string, subdomainFlag bool, records [][]string) []string {
+	var ips []string
+
+	if subdomainFlag {
+		for _, record := range records {
+			subdomain := record[0]
+			ip := record[1]
+			isCloudflare := record[2]
+
+			if subdomain == domain && !strings.EqualFold(isCloudflare, "true") {
+				ips = append(ips, ip)
+			}
+		}
+	} else {
+		for _, record := range records {
+			recordDomain := record[0]
+			if recordDomain == domain {
+				ip := record[1]
+				ips = append(ips, ip)
+			}
+		}
+	}
+
+	return ips
+}
+
+func fetchHeaders(domain string, useHTTPS bool) (http.Header, error) {
+	scheme := "http"
+	if useHTTPS {
+		scheme = "https"
+	}
+
+	url := fmt.Sprintf("%s://%s", scheme, domain)
+	response, err := http.Head(url)
 	if err != nil {
 		return nil, err
 	}
@@ -124,13 +261,24 @@ func fetchHeaders(domain string, port int) (http.Header, error) {
 	return response.Header, nil
 }
 
-func fetchResponseBody(domain string, port int) ([]byte, error) {
-	response, err := http.Get(fmt.Sprintf("http://%s:%d", domain, port))
+func fetchResponseBody(domain string, useHTTPS bool) ([]byte, error) {
+	scheme := "http"
+	if useHTTPS {
+		scheme = "https"
+	}
+
+	response, err := http.Get(fmt.Sprintf("%s://%s", scheme, domain))
 	if err != nil {
 		return nil, err
 	}
 	defer response.Body.Close()
-	return ioutil.ReadAll(response.Body)
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
 
 func isWordPress(body []byte) bool {
